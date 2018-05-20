3051f3d2de15d65cfc793bf212c6b8ddad6c71a8
southernchelseafan <34219355+southernchelseafan@users.noreply.github.com>
2018-05-20 10:32:01 -0500

    Revert "Starting node express project."
    This reverts commit 1916c235f8cb3b0b5983c9f9a412718b35e2fca4.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Node Stock Quote
-
-Simple node express demo using remote API for stock quotes & a local mongodb for storing recent quotes.
--- models/stock.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var StockSchema = new Schema({
-  symbol: String,
-  quote: Object
-});
-
-module.exports = mongoose.model('Stock', StockSchema);
